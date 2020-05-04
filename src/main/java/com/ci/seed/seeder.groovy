c9547ecb1c5de3450232483059e1e9aa8965e327
Sakthiganesh R <Sakthiganesh_R@Trimble.com>
2020-05-04 09:46:45 +0530

    might be faulty

--- src/main/java/com/ci/seed/seeder.groovy
+++ src/main/java/com/ci/seed/seeder.groovy
@@ -1,12 +1,5 @@
 package com.ci.seed
 
-import org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval
-
-ScriptApproval scriptApproval = ScriptApproval.get()
-scriptApproval.pendingScripts.each {
-    scriptApproval.approveScript(it.hash)
-}
-
 job("Seeder (Master Job)") {
 
     scm {
